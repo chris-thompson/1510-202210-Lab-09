8d54ac5beaac93222d2a12440bfcf80e6f3b3aab
Chris <chris_thompson@bcit.ca>
2022-03-25 17:34:44 -0700

    Hello world!

---
+++ exceptions.py
@@ -0,0 +1,27 @@
+"""
+Have fun!
+"""
+
+
+def heron(number):
+    pass
+
+
+def find_an_even(input_list):
+    """
+    Return the first even number in input_list.
+
+    param input_list: a list of integers
+    precondition: input_list must be a list of integer
+    post-condition: return the first even number in input_list
+    raises ValueError: if input_list does not contain an even number
+    return: first even number in input_list
+    """
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
